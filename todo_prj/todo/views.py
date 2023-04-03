e0c19ec20a2c143db646bf9326d8c615b684a021
Sema <sema78.work@gmail.com>
2023-04-03 22:00:57 +0200

    ToDoItem model added. ToDoItem form added. 'todo' app added to poject settings.py

--- todo_prj/todo/views.py
+++ todo_prj/todo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .forms import ToDoItemForm
 from .models import ToDoItem
 
+
 # Create your views here.
 def todo_list(request):
     todos = ToDoItem.objects.all()
@@ -14,8 +15,13 @@ def todo_list(request):
     else:
         form = ToDoItemForm()
     context = {'todos': todos, 'form': form}
+    if request.method == 'POST' and 'delete_id' in request.POST:
+        todo_id =int(request.POST.get('delete_id'))
+        todo = ToDoItem.objects.get(id=todo_id)
+        todo.delete()
+        return redirect('todo_list')
     if 'completed_id' in request.POST:
         completed_todo = ToDoItem.objects.get(pk=request.POST['completed_id'])
         completed_todo.completed = True
         completed_todo.save()
-    return render(request, 'todos/todo_list.html', context)
+    return render(request, 'todo/todo_list.html', context)
